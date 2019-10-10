776b83b09f50b384010b5cb3739051d7d906a4a4
Fabian Pechmann <6319@htl.rennweg.at>
2019-10-10 13:19:40 +0200

    v0.1  Navbar Prototyp erstellt

---
+++ app/components/components.js
@@ -0,0 +1,11 @@
+"use strict";
+
+app.component("ProfileComponent", {
+    templateUrl: "components/profile-component.html",
+    controller: "ProfileComponentController"
+});
+
+app.controller("ProfileComponentController", function () {
+    this.title = "Profile Tab";
+    this.description = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+});
