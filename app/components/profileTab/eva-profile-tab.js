b768846ea8baa99de76bc2901d96133fd32ad682
moritzsattl <6323@htl.rennweg.at>
2019-11-22 21:23:32 +0100

    improve profile tab

--- app/components/profileTab/eva-profile-tab.js
+++ app/components/profileTab/eva-profile-tab.js
@@ -11,12 +11,13 @@ app.component("evaProfileTab",{
 
 app.controller("ProfileController", function($scope){
     $scope.profileSrc = "images/logo-eva-4.png";
-    $scope.profileName= "Eva-App";
-    $scope.followers = 1224;
-    $scope.following = 100;
+    $scope.profileName= "triff.";
+    $scope.eventcount= 4;
+    $scope.followers = 765;
+    $scope.following = 672;
     $scope.profileBio = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.";
-    $scope.achievments = ["images/achievements/achievement1.png",
-                        "images/achievements/achievment2.jpg",
-                        "images/achievements/achievment3.png",
-                        "images/achievements/achievment4.png"];
+    $scope.events=["Notorious",
+        "prestige","Moritzevent","Fabianevent"];
+    /*Alle erstellten Events aufgelistet*/
+    /*$scope.events*/
 });
