d92d4862a82b4c3da5924991008bf229bd77c02f
moritzsattl <6323@htl.rennweg.at>
2019-12-19 11:48:46 +0100

    add user factory

--- app/components/trendingEventTab/eva-event-tab.js
+++ app/components/trendingEventTab/eva-event-tab.js
@@ -4,11 +4,20 @@ app.component("evaEventTab", {
     templateUrl: "components/trendingEventTab/eva-event-tab.html",
     controller: "EventController",
     bindings: {
-
+        event: '<'
     }
 });
 
-app.controller("EventController", function ($scope,eventService) {
+app.controller("EventController", function ($scope,eventService,usersService,$log) {
+    $log.debug("EventController");
+    console.log(usersService.users);
+    $scope.veranstalter = ()=>{
+        return eventService.events;
+    };
+    $scope.users = () =>{
+
+        return usersService.users;
+    };
     $scope.events = () =>{
         return eventService.events;
     }
---
+++ app/services/users-service.js
@@ -0,0 +1,7 @@
+app.service("usersService",function(User){
+    this.users = [
+        new User("Fabian","images/sample_pic.jpg",4,234,2),
+        new User("Fabian","images/sample_pic.jpg",4,234,2),
+        new User("Fabian","images/sample_pic.jpg",4,234,2)
+    ];
+});
