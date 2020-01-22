7304ae497f02db75110ba9ded8706e36872a6498
Utku Uelker <6332@htl.rennweg.at>
2020-01-22 18:54:09 +0100

    fix follow function php

--- app/components/trendingEventTab/eva-profile-info.php
+++ app/components/trendingEventTab/eva-profile-info.php
@@ -11,6 +11,10 @@ try {
 }
 
 $userId = $_SESSION['user']['pk_user_id'];
+
+$stmt = $db->query("SELECT COUNT(*) AS 'Anzahl' FROM follows WHERE fk_user_id = '$userId'");
+$already_follows = $stmt->fetch(PDO::FETCH_ASSOC);
+
 $username = $_POST['username'];
 
 $stmt = $db->query("SELECT * FROM User WHERE username = '$username'");
@@ -19,5 +23,13 @@ $follow_me = $stmt->fetch(PDO::FETCH_ASSOC);
 
 $follow_me_id = $follow_me['pk_user_id'];
 
-$stmt = $db->query("INSERT INTO follows VALUES('$userId', '$follow_me_id');");
-$stmt = $db->query("INSERT INTO followed_By VALUES('$follow_me_id', '$userId');");
+if ($already_follows['Anzahl'] > 0){
+
+    $stmt = $db->query("DELETE FROM follows WHERE fk_user_id = '$userId' AND fk_follows_id = '$follow_me_id'");
+    $stmt = $db->query("DELETE FROM followed_by WHERE fk_user_id = '$follow_me_id' AND fk_followed_by_id ='$userId'");
+
+} else {
+
+    $stmt = $db->query("INSERT INTO follows VALUES('$userId', '$follow_me_id');");
+    $stmt = $db->query("INSERT INTO followed_by VALUES('$follow_me_id', '$userId');");
+}
